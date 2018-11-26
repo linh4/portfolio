033eb76c7c1ae72e157080c9e137d43be716660e
linh4 <linh44h@gmail.com>
2018-11-25 22:13:26 -0500

    changed li a

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
 
-  $("nav > ul li a").click(function(e){
+  $("nav > ul li").click(function(e){
+    e.stopPropagation()
     e.preventDefault()
     $('.active').not($(this).closest('li').addClass('active')).removeClass('active')
     if ($(".have-children").hasClass("active")) {
       $(".sidebar nav").toggleClass("active");
-      $(".have-children a").next().slideToggle()
+      $(".have-children span").next().slideToggle()
       $('.page:visible').show();
     }
     else if ($(".have-children").find('li').hasClass('active')) {
-      $('.page:visible').fadeOut(50);
-      $('.page[id=' + $(this).data('id') + ']').fadeIn(50);
+      $('.page:visible').hide('fast');
+      $('.page[id=' + $(this).data('id') + ']').show('fast');
     }
     else {
-      $(this).next().slideToggle();
-      $('.page:visible').fadeOut(50);
-      $('.page[id=' + $(this).data('id') + ']').fadeIn(50);
+      $('.page:visible').hide('fast');
+      $('.page[id=' + $(this).data('id') + ']').show('fast');
     }
   })
 
@@ -28,9 +28,15 @@ $(document).ready(function(){
        $("nav").toggleClass("active");
    })
 
-
-
-
+   let typed = new Typed("#typed", {
+      strings: ["<i>I'm a full-stack developer</i>.","<i>I'm passionate about front-end development</i>.", "<i>I'm a homechef</i>.", "<i>I love cats</i>.", "<i>I'm based in NYC."],
+      starDelay: 300,
+      backDelay: 600,
+      typeSpeed: 60,
+      loop: true,
+      showCursor: true,
+      cursorChar: "|",
+    });
 
 
 });
